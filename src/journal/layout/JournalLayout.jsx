90e281370aa02e5e630c8ec377f68635c1124022
StevenDev <bossiolealbl@gmail.com>
2024-07-11 14:36:22 -0500

    feat: sideBar component

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,20 +1,18 @@
 import { Box } from "@mui/system"
-import { NavBar } from "../components"
+import { NavBar, SideBar } from "../components"
+import { Toolbar } from "@mui/material";
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({children}) => {
   return (
-    <Box sx={{display:'flex'}}>
-
+    <Box sx={{ display: "flex" }}>
       <NavBar drawerWidth={drawerWidth} />
-
-        <Box
-            component='main'
-            sx={{flexGrow:1 , p:3 }}
-        >
-          {children}
-        </Box>
+      <SideBar drawerWidth={drawerWidth} />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        {children}
+      </Box>
     </Box>
-  )
+  );
 }
