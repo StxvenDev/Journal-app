996bd4cc07ad7d13fcdf9a1dc45ea0f885a92724
StevenDev <bossiolealbl@gmail.com>
2024-07-12 15:39:08 -0500

    feat: float button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,8 @@
 
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteViews, NothingSelectedView } from "../views";
+import { AddOutlined } from "@mui/icons-material";
 
 const drawerWidth = 240;
 
@@ -8,8 +10,20 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad provident soluta fugiat ex! Architecto reiciendis nam molestiae ab, eum distinctio id nihil, praesentium numquam veniam, ipsum assumenda quasi quo enim.</Typography> */}
-      {/* <NothingSelectedView /> */}
-      <NoteViews />
+      <NothingSelectedView />
+      {/* <NoteViews /> */}
+      <IconButton
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize:30}} />
+      </IconButton>
     </JournalLayout>
   )
 }
