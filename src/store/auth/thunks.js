92b9e822e3d9672289eafe6560dcc4d0df92f723
StevenDev <bossiolealbl@gmail.com>
2024-07-17 17:58:21 -0500

    config: firebase google oauth initial configuration

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { signInWithGoogle } from "../../firebase/providers";
 import { chekingCredentials } from "./authSlice"
 
 export const chekingAuthentication = (email, password) => {
@@ -9,5 +10,8 @@ export const chekingAuthentication = (email, password) => {
 export const startCheckingGoogle = () => {
     return async (dispatch) => {
         dispatch(chekingCredentials());
+        const result = await signInWithGoogle();
+        console.log({result});
+        
     }
 }
